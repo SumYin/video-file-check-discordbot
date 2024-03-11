626dbd0f0f22282512d3f9b9d13feea02698987b
SumYin <ryanzhang333@gmail.com>
2024-03-11 23:28:53 +0100

    Add Error Message Logic
    Changed
    -Moved Try Except to main bot file and not the function file
    
    Added
    -Added send error message

--- main.py
+++ main.py
@@ -17,19 +17,30 @@ async def ping(interaction: nextcord.Interaction):
 
 @bot.slash_command(description="check media file data")
 async def check(interaction: nextcord.Interaction, attached_file:nextcord.Attachment):
-    file_path=await download_video(attached_file)
-    returned_data = await check_video(file_path)
-
-    embed=nextcord.Embed(title=f"{attached_file.filename}", color=0x00ff00)
-    embed.add_field(name="Size (MB)", value=returned_data["file_size"], inline=False)
-    embed.add_field(name="Type", value=returned_data["file_type"], inline=False)
-    embed.add_field(name="Resolution", value=returned_data["file_resolution"], inline=False)
-    embed.add_field(name="Frame Count", value=returned_data["file_framecount"], inline=False)
-    embed.add_field(name="Frame Rate", value=returned_data["file_framerate"], inline=False)
-    embed.add_field(name="Codec", value=returned_data["file_codec"], inline=False)
-    embed.set_image(url=attached_file.url)
-    embed.set_footer(text=interaction.user.name, icon_url=interaction.user.avatar.url)
-
-    await interaction.send(embed=embed, ephemeral=False)
+    try:
+        #calling function to download the video
+        file_path=await download_video(attached_file)
+    except Exception as e:
+        print(f"Error downloading video: {str(e)}")
+        return await interaction.send("Error downloading video", ephemeral=True)
+    
+    try:
+        #calling function to get the media data
+        returned_data = await check_video(file_path)
+        
+        #creating embed to send
+        embed=nextcord.Embed(title=f"{attached_file.filename}", color=0x00ff00)
+        embed.add_field(name="Size (MB)", value=returned_data["file_size"], inline=False)
+        embed.add_field(name="Type", value=returned_data["file_type"], inline=False)
+        embed.add_field(name="Resolution", value=returned_data["file_resolution"], inline=False)
+        embed.add_field(name="Frame Count", value=returned_data["file_framecount"], inline=False)
+        embed.add_field(name="Frame Rate", value=returned_data["file_framerate"], inline=False)
+        embed.add_field(name="Codec", value=returned_data["file_codec"], inline=False)
+        embed.set_image(url=attached_file.url)
+        embed.set_footer(text=interaction.user.name, icon_url=interaction.user.avatar.url)
+        await interaction.send(embed=embed, ephemeral=False)
+    except Exception as e:
+        print(f"Error checking video: {str(e)}")
+        return await interaction.send("Error checking video", ephemeral=True)
 
 bot.run(os.getenv("TOKEN"))
--- media_check.py
+++ media_check.py
@@ -6,20 +6,16 @@ import aiohttp
 
 # file download
 async def download_video(attached_file):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(attached_file.url) as response:
-                file_path = os.path.join("./videos", f"{attached_file.id}_{os.path.basename(attached_file.filename).split('?')[0]}")
-                with open(file_path, "wb") as file:
-                    while True:
-                        chunk = await response.content.read(1024)
-                        if not chunk:
-                            break
-                        file.write(chunk)
-        return file_path
-    except Exception as e:
-        print(e)
-        return f"Error downloading video: {str(e)}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(attached_file.url) as response:
+            file_path = os.path.join("./videos", f"{attached_file.id}_{os.path.basename(attached_file.filename).split('?')[0]}")
+            with open(file_path, "wb") as file:
+                while True:
+                    chunk = await response.content.read(1024)
+                    if not chunk:
+                        break
+                    file.write(chunk)
+    return file_path
 
 # data class for ffprobe output
 class FFProbeResult(NamedTuple):
@@ -45,41 +41,36 @@ def ffprobe(file_path) -> FFProbeResult:
 
 #check/return video data
 async def check_video(file_path):
-    try:
-        videoProperties = {}
-        
-        data = json.loads(ffprobe(file_path).json)
-        streams = data.get("streams", [])
-
-        for stream in streams:
-            if stream.get("codec_type") == "video":
-
-                # file size (float)
-                videoProperties["file_size"] = round(os.stat(file_path).st_size / (1024 * 1024), 2)
-
-                # file type (string)
-                videoProperties["file_type"] = str(os.path.splitext(file_path)[1])
-
-                # resolution (int, int)
-                videoProperties["file_resolution"] = (int(stream.get("width", 0)), int(stream.get("height", 0)))
-
-                # frame count (int)
-                frameCount = int(stream.get("nb_read_frames", 0))
-                videoProperties["file_framecount"] = frameCount
-
-                # frame rate (float)
-                try:
-                    fps = round(float(int(stream.get("nb_read_frames", 0)) / float(stream.get("duration", 0))), 2)
-                    videoProperties["file_framerate"] = fps
-                except:
-                    videoProperties["file_framerate"] = "Null"
+    videoProperties = {}
     
-                # codec (string)
-                codec = str(stream.get("codec_name", "unknown"))
-                videoProperties["file_codec"] = codec
-    
-        return videoProperties
-    
-    except Exception as e:
-        print(e)
-        raise e
+    data = json.loads(ffprobe(file_path).json)
+    streams = data.get("streams", [])
+
+    for stream in streams:
+        if stream.get("codec_type") == "video":
+
+            # file size (float)
+            videoProperties["file_size"] = round(os.stat(file_path).st_size / (1024 * 1024), 2)
+
+            # file type (string)
+            videoProperties["file_type"] = str(os.path.splitext(file_path)[1])
+
+            # resolution (int, int)
+            videoProperties["file_resolution"] = (int(stream.get("width", 0)), int(stream.get("height", 0)))
+
+            # frame count (int)
+            frameCount = int(stream.get("nb_read_frames", 0))
+            videoProperties["file_framecount"] = frameCount
+
+            # frame rate (float)
+            try:
+                fps = round(float(int(stream.get("nb_read_frames", 0)) / float(stream.get("duration", 0))), 2)
+                videoProperties["file_framerate"] = fps
+            except:
+                videoProperties["file_framerate"] = "Null"
+
+            # codec (string)
+            codec = str(stream.get("codec_name", "unknown"))
+            videoProperties["file_codec"] = codec
+
+    return videoProperties
