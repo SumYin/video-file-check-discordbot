67ada9b50669f6d147511f60d4221b109204ab92
vi <reducedprocess@gmail.com>
2024-03-14 00:12:52 -0400

    cooldown formatting, fix dependency name

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
+
 import cooldowns
 from cooldowns import CallableOnCooldown
 
@@ -17,6 +18,10 @@ load_dotenv()
 # get user data
 f = open('user.json')
 user = json.load(f)
+cooldown_short_tokens = user.get("cooldown_times").get("short").get("tokens", 1)
+cooldown_short_period = user.get("cooldown_times").get("short").get("period", 5)
+cooldown_long_tokens = user.get("cooldown_times").get("long").get("tokens", 1)
+cooldown_long_period = user.get("cooldown_times").get("long").get("period", 20)
 
 # discord bot
 intents = nextcord.Intents.default()
@@ -29,29 +34,30 @@ async def on_ready():
 @bot.event
 async def on_application_command_error(interactions: Interaction, error):
     error = getattr(error, "original", error)
+    # cooldown error
     if isinstance(error, CallableOnCooldown):
         embed=nextcord.Embed(title="Cooldown", description=(f"You are being rate-limited! Retry in `{error.retry_after}` seconds."), color=0xff0000)
         await interactions.send(embed=embed, ephemeral=True)
+    
+    # permissions error
+    elif isinstance(error, PermissionError): await interactions.send("Permissions error.")
+    else: pass
 
-    elif isinstance(error, PermissionError):
-        await interactions.send(
-            f"PermissionError"
-        )
-    else:
-        pass
-
+# ping command
 @bot.slash_command(description="test ping")
-@cooldowns.cooldown(user["cooldown_times"]["short"][0], user["cooldown_times"]["short"][1], bucket=cooldowns.SlashBucket.author)
+@cooldowns.cooldown(cooldown_short_tokens, cooldown_short_period, bucket=cooldowns.SlashBucket.author)
 async def ping(interaction: nextcord.Interaction):
     await interaction.send("Pong!", ephemeral=True)
 
 # info command
 @bot.slash_command(description="get information about this bot")
+@cooldowns.cooldown(cooldown_short_tokens, cooldown_short_period, bucket=cooldowns.SlashBucket.author)
 async def info(interaction: nextcord.Interaction):
     await interaction.send("Bot Source Code: https://github.com/SumYin/video-file-check-discordbot", ephemeral=True)
 
 # rules command
 @bot.slash_command(description="check rules")
+@cooldowns.cooldown(cooldown_short_tokens, cooldown_short_period, bucket=cooldowns.SlashBucket.author)
 async def rules(interaction: nextcord.Interaction):
     embed=nextcord.Embed(title=":scroll: __Rules__ | __read carefully__ :scroll:", color=0x000000, description="")
     for rule in user.get("rules"):
@@ -60,6 +66,7 @@ async def rules(interaction: nextcord.Interaction):
 
 # faq command
 @bot.slash_command(description="check frequently asked questions")
+@cooldowns.cooldown(cooldown_short_tokens, cooldown_short_period, bucket=cooldowns.SlashBucket.author)
 async def faq(interaction: nextcord.Interaction):
     embed=nextcord.Embed(title=":grey_question: __Frequently Asked Questions__ :grey_question:", color=0x00000)
     for faq in user.get("faq"):
@@ -68,7 +75,7 @@ async def faq(interaction: nextcord.Interaction):
 
 # attachment command
 @bot.slash_command(description="check attached file data")
-@cooldowns.cooldown(user["cooldown_times"]["long"][0], user["cooldown_times"]["long"][1], bucket=cooldowns.SlashBucket.author)
+@cooldowns.cooldown(cooldown_long_tokens, cooldown_long_period, bucket=cooldowns.SlashBucket.author)
 async def check_file(interaction: nextcord.Interaction, attached_file:nextcord.Attachment):
     await interaction.response.defer(ephemeral=True) # we need to decide if ephemeral when we defer, we can't affect this later
     file_path=await download_attachment(attached_file)
@@ -76,7 +83,7 @@ async def check_file(interaction: nextcord.Interaction, attached_file:nextcord.A
     
 # link commmand
 @bot.slash_command(description="check link file data")
-@cooldowns.cooldown(user["cooldown_times"]["long"][0], user["cooldown_times"]["long"][1], bucket=cooldowns.SlashBucket.author)
+@cooldowns.cooldown(cooldown_long_tokens, cooldown_long_period, bucket=cooldowns.SlashBucket.author)
 async def check_link(interaction: nextcord.Interaction, file_link: str):
     await interaction.response.defer(ephemeral=True) # we need to decide if ephemeral when we defer, we can't affect this later
     file_path=await download_link(file_link, interaction.user.id)
@@ -85,7 +92,7 @@ async def check_link(interaction: nextcord.Interaction, file_link: str):
 # analyze file and output data
 async def produce_embed(file_path, interaction, file_name):
     try:
-        returned_data = await check_video(file_path)
+        video_data = await check_video(file_path)
         
         embed=nextcord.Embed(title=file_name, color=0x000000, timestamp=datetime.now())
 
@@ -96,38 +103,38 @@ async def produce_embed(file_path, interaction, file_name):
         # size
         check = invalid
         for prop in user.get("targets").get("size"):
-            if returned_data["file_size"] < prop: check = valid
-        embed.add_field(name="Size (MB)", value=str(returned_data["file_size"]) + check, inline=False)
+            if video_data["file_size"] < prop: check = valid
+        embed.add_field(name="Size (MB)", value=str(video_data["file_size"]) + check, inline=False)
 
         # type
         check = invalid
         for prop in user.get("targets").get("type"):
-            if returned_data["file_type"] == prop: check = valid
-        embed.add_field(name="Type", value=str(returned_data["file_type"]) + check, inline=False)
+            if video_data["file_type"] == prop: check = valid
+        embed.add_field(name="Type", value=str(video_data["file_type"]) + check, inline=False)
 
         # resolution
         check = invalid
         for prop in user.get("targets").get("resolution"):
-            if returned_data["file_resolution"] == prop: check = valid
-        embed.add_field(name="Resolution", value=str(returned_data["file_resolution"]) + check, inline=False)
+            if video_data["file_resolution"] == prop: check = valid
+        embed.add_field(name="Resolution", value=str(video_data["file_resolution"]) + check, inline=False)
 
         # frame count
         check = invalid
         for prop in user.get("targets").get("framecount"):
-            if returned_data["file_framecount"] == prop: check = valid
-        embed.add_field(name="Frame Count", value=str(returned_data["file_framecount"]) + check, inline=False)
+            if video_data["file_framecount"] == prop: check = valid
+        embed.add_field(name="Frame Count", value=str(video_data["file_framecount"]) + check, inline=False)
 
         # frame rate
         check = invalid
         for prop in user.get("targets").get("framerate"):
-            if returned_data["file_framerate"] == prop: check = valid
-        embed.add_field(name="Frame Rate", value=str(returned_data["file_framerate"]) + check, inline=False)
+            if video_data["file_framerate"] == prop: check = valid
+        embed.add_field(name="Frame Rate", value=str(video_data["file_framerate"]) + check, inline=False)
 
         # codec
         check = invalid
         for prop in user.get("targets").get("codec"):
-            if returned_data["file_codec"] == prop: check = valid
-        embed.add_field(name="Codec", value=str(returned_data["file_codec"]) + check, inline=False)
+            if video_data["file_codec"] == prop: check = valid
+        embed.add_field(name="Codec", value=str(video_data["file_codec"]) + check, inline=False)
 
         embed.set_footer(text=interaction.user.name, icon_url=interaction.user.avatar.url)
         await interaction.followup.send(embed=embed)
